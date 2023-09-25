c6af6354a8b18623b47d0a008a28da93380d679c
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2023-09-24 21:51:06 -0300

    se eliminan comentarios

--- controllers/carts.js
+++ controllers/carts.js
@@ -1,7 +1,6 @@
 const CartModel = require("../modals/cart")
 const ProductModel = require("../modals/products")
 
-/* Trae un carrito con todos los productos: */
 const getOneCartAllProduct = async (req, res) => {
     try {
         const getCart = await CartModel.findOne({_id: req.params.id})
@@ -16,19 +15,14 @@ const addProductCart = async (req, res) => {
        const getCart = await CartModel.findOne({_id: req.params.idCart}) /* Busco el carrito */
        const getProd = await ProductModel.findOne({_id: req.params.idProd})/* busco el producto */
       
-       /* AGREGUE HOY */
       const prodExist = getCart.products.filter((prod) => prod._id == req.params.idProd)
-      console.log(prodExist)
 
       if(prodExist.length > 0){
         return res.status(400).json({msg: 'Producto duplicado en su carrito', status: 400})
       }
- 
-      
- /* FIN */
 
-       getCart.products.push(getProd) /* agrego el producto al carrito */
-       await getCart.save()   /* guarda el carrito */                             
+       getCart.products.push(getProd) 
+       await getCart.save()                             
        res.status(200).json({msg: 'El producto se cargo en el carrito correctamente', getCart})
       } catch (error) {
       console.log(error)
